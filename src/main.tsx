8188a8dcbafe434be4b8ef90c6805d5f6aab7985
sonnenjov <sonnenjov@gmail.com>
2026-06-18 17:05:51 +0200

    updated css

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 // main.tsx
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import App_Routes from './routes.tsx'
 import { Season, useSeason } from './hooks/useSeason.ts'
 import { JSX } from 'react'
 import './i18n'
 import {AuthProvider}  from './hooks/useAuth.tsx'
 import { AppDataProvider } from './hooks/useAppData.tsx'
+import './index.css'
 export function App(): JSX.Element {
   return (
     <BrowserRouter>
