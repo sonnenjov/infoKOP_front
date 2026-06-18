2ec73923effd343f6215c7c69b56caac2b1b69fc
sonnenjov <sonnenjov@gmail.com>
2026-06-18 14:41:00 +0200

    Disable CSS minify to fix lightningcss keyframes bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    cssMinify: false,
+  },
   plugins: [react()],
   base: '/',  // For root deployment
-  build: {
-    outDir: 'dist',  // This should match your build folder
-  },
 })
